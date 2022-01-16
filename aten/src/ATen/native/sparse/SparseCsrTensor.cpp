ee1074f54896ac259924a8ecc127596c2d150f82
Christian Puhrsch <cpuhrsch@fb.com>
2022-01-16 20:38:55 +0000

    Add block sparse support to sparse_csr_tensor constructor

--- aten/src/ATen/native/sparse/SparseCsrTensor.cpp
+++ aten/src/ATen/native/sparse/SparseCsrTensor.cpp
@@ -34,10 +34,12 @@ void _validate_sparse_csr_tensor_args(const Tensor& crow_indices, const Tensor& 
       values.layout() == kStrided && values.is_contiguous(),
       "expected values to be a strided and contiguous tensor");
 
+  bool is_block_sparse = (values.dim() == 3);
+
   // Shape and Strides invariants
   TORCH_CHECK(
       size.size() == 2,
-      "size of a CSR tensor must be of length 2, but got: ",
+      "dimension of a CSR tensor must be 2, but got: ",
       size.size());
   TORCH_CHECK(
       crow_indices.dim() == 1,
@@ -48,20 +50,54 @@ void _validate_sparse_csr_tensor_args(const Tensor& crow_indices, const Tensor& 
       "col_indices must have dim=1 but got col_indices.dim()=",
       col_indices.dim());
   TORCH_CHECK(
-      values.dim() == 1,
-      "values must have dim=1 but got values.dim()=",
+      values.dim() == 1 || is_block_sparse,
+      "values must have dim=1 or dim=3 but got values.dim()=",
       values.dim());
-  // Note, this check also enforces `crow_indices.numel() >= 1`
-  TORCH_CHECK(
-      crow_indices.numel() == (size[0] + 1),
-      "crow_indices.numel() must be size(0) + 1, but got: ",
-      crow_indices.numel());
-  TORCH_CHECK(
-      col_indices.numel() == values.numel(),
-      "col_indices and values must have equal sizes, but got col_indices.numel(): ",
-      col_indices.numel(),
-      ", values.numel(): ",
-      values.numel());
+  if (is_block_sparse) {
+    IntArrayRef block_size({values.size()[1], values.size()[2]});
+    bool block_numel = block_size[0] * block_size[1];
+    TORCH_CHECK(block_size[0] == block_size[1] && block_size[0] > 1,
+        "For block sparse CSR Tensors (3-dim values) the ",
+        "blocks must be square and greater than 1.",
+        "Got (",
+        block_size[0],
+        ", ",
+        block_size[1],
+        ") instead.");
+    TORCH_CHECK(block_size[0] == block_size[1] && block_size[0] > 1,
+        "Block sparse CSR Tensors must have a size that is an integral multiple of their block size.",
+        "Got (",
+        size[0],
+        ", ",
+        size[1],
+        ") with block size (",
+        block_size[0],
+        ", ",
+        block_size[1],
+        ") instead.");
+    TORCH_CHECK(
+        crow_indices.numel() == (size[0] / block_size[0] + 1),
+        "crow_indices.numel() must be size(0) / size(1) + 1, but got: ",
+        crow_indices.numel());
+    TORCH_CHECK(
+        col_indices.numel() == values.size[0],
+        "col_indices and values leading size must have be equal, but got col_indices.numel(): ",
+        col_indices.numel(),
+        ", values.size(0): ",
+        values.size(0));
+  } else {
+    // Note, this check also enforces `crow_indices.numel() >= 1`
+    TORCH_CHECK(
+        crow_indices.numel() == (size[0] + 1),
+        "crow_indices.numel() must be size(0) + 1, but got: ",
+        crow_indices.numel());
+    TORCH_CHECK(
+        col_indices.numel() == values.numel(),
+        "col_indices and values must have equal sizes, but got col_indices.numel(): ",
+        col_indices.numel(),
+        ", values.numel(): ",
+        values.numel());
+  }
 
   // Indices invariants
   AT_DISPATCH_INDEX_TYPES(crow_indices.scalar_type(), "csr_construct_check", [&] {
@@ -81,7 +117,11 @@ void _validate_sparse_csr_tensor_args(const Tensor& crow_indices, const Tensor& 
     }
     if (col_indices.numel() > 0) {
       TORCH_CHECK(0 <= col_indices.min().item<index_t>(), "col_indices.min() should be greater or equal to zero");
-      TORCH_CHECK(size[1] > col_indices.max().item<index_t>(), "size(1) should be greater than col_indices.max()");
+      if (is_block_sparse) {
+        TORCH_CHECK((size[1] / block_size[0] )> col_indices.max().item<index_t>(), "size(1) / values.size(0) should be greater than col_indices.max()");
+      } else {
+        TORCH_CHECK(size[1] > col_indices.max().item<index_t>(), "size(1) should be greater than col_indices.max()");
+      }
     }
   });
 
@@ -115,11 +155,12 @@ void _validate_sparse_csr_tensor_args(const Tensor& crow_indices, const Tensor& 
       values.device().type() == kCPU || values.device().type() == kCUDA,
       "device type of values (",
       values.device().type(),
-      ") must be CPU or CUDA");
+      ") must be CPU or CUDA ",
+      "but got ", values.device(), "instead.");
 }
 
 // Construction of CSR tensors.
-SparseCsrTensor new_csr_tensor(const TensorOptions& options) {
+SparseCsrTensor _new_csr_tensor(const TensorOptions& options) {
   // TODO: remove this comment after enabling autograd support for CSR tensor
   // constructor.
   // TORCH_INTERNAL_ASSERT(impl::variable_excluded_from_dispatch());
@@ -150,7 +191,7 @@ Tensor _sparse_csr_tensor_unsafe(const Tensor& crow_indices, const Tensor& col_i
 
   TensorOptions options = TensorOptions().dtype(dtype).layout(layout).device(device).pinned_memory(pin_memory);
 
-  SparseCsrTensor self = new_csr_tensor(options);
+  SparseCsrTensor self = _new_csr_tensor(options);
   get_sparse_csr_impl(self)->set_member_tensors(crow_indices, col_indices, values, size);
   return self;
 }
